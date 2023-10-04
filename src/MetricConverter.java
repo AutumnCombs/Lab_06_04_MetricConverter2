aeffba50e66bf16a863efd0f4720333a5d998913
AutumnCombs <combsau@mail.uc.edu>
2023-10-03 22:45:07 -0400

    finished task4

--- src/MetricConverter.java
+++ src/MetricConverter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MeasurementConverter {
+public class MetricConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
